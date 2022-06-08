acd44fcade0c0db2e63599e098ff2eb95c86180c
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-06-08 16:26:27 +0300

    feat: add basic site navigation by withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, subtitle, imageUrl, size = "" }) => (
-  <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size = "", linkUrl, history, match }) => (
+  <div
+    className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
     ></div>
     <div className="content">
       <h1 className="title">{title}</h1>
-      <span className="subtitle">{subtitle}</span>
+      <span className="subtitle">shop now</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
